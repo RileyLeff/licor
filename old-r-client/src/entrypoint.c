52949ec637e663f24a6c795e170bfa710d752999
RileyLeff <rileyleff@gmail.com>
2025-06-04 18:33:49 -0400

    removed junk scripts

--- old-r-client/src/entrypoint.c
+++
@@ -1,8 +0,0 @@
-// We need to forward routine registration from C to Rust
-// to avoid the linker removing the static library.
-
-void R_init_licorclient_extendr(void *dll);
-
-void R_init_licorclient(void *dll) {
-    R_init_licorclient_extendr(dll);
-}
--- old-r-client/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(licorclient)
-
-test_check("licorclient")
